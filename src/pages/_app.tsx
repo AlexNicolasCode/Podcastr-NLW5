6b945c0cb8bd07c040ee27d88a6175a9b564869b
Alex <alexoliveira604@gmail.com>
2021-04-22 20:37:32 -0300

    aula 4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,49 @@
 import Player from '../components/Player/index';
 import Header from '../components/Header/index';
+import { PlayerContext } from '../contexts/PlayerContext';
+
 import '../components/styles/global.scss';
 import '../components/styles/app.module.scss';
 import styles from '../components/styles/app.module.scss'
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying)
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={{
+      episodeList, 
+      currentEpisodeIndex, 
+      play, 
+      isPlaying, 
+      togglePlay,
+      setPlayingState 
+      }}>
+      
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   )
 }
 
--- src/pages/episodes/{slug}.tsx
+++ src/pages/episodes/{slug}.tsx
@@ -64,9 +64,25 @@ export default function Episode( {episode}: EpisodeProps ) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    const { data } = await api.get('episodes', {
+        params: {
+            _linit: 2,
+            _sort: 'published_at',
+            _order: 'desc'
+        }
+    })
+
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: data.id
+            }
+        }
+    })
+
     return {
-        paths: [],
-        fallback: 'blocking' 
+        paths,
+        fallback: 'blocking'
     }
 }
 
